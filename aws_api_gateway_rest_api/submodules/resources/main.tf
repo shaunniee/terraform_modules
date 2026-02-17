aed236e3d5c622076e3f87c829c685ee832132f3
Shaun Vivian Dsouza <shaunvividsz@gmail.com>
2026-02-17 21:27:33 +0000

    fix bug with api-gw

--- aws_api_gateway_rest_api/submodules/resources/main.tf
+++ aws_api_gateway_rest_api/submodules/resources/main.tf
@@ -1,17 +1,54 @@
-resource "aws_api_gateway_resource" "this" {
-  for_each = var.resources
+locals {
+  level_1 = { for k, v in var.resources : k => v if try(v.parent_key, null) == null }
 
+  level_2 = { for k, v in var.resources : k => v
+    if try(v.parent_key, null) != null &&
+    contains(keys(local.level_1), v.parent_key) }
+
+  level_3 = { for k, v in var.resources : k => v
+    if try(v.parent_key, null) != null &&
+    contains(keys(local.level_2), v.parent_key) }
+
+  level_4 = { for k, v in var.resources : k => v
+    if try(v.parent_key, null) != null &&
+    contains(keys(local.level_3), v.parent_key) }
+
+  level_5 = { for k, v in var.resources : k => v
+    if try(v.parent_key, null) != null &&
+    contains(keys(local.level_4), v.parent_key) }
+}
+
+resource "aws_api_gateway_resource" "level_1" {
+  for_each    = local.level_1
   rest_api_id = var.rest_api_id
-  parent_id   = local.resource_ids_with_root[coalesce(try(each.value.parent_key, null), "__root__")]
+  parent_id   = var.root_resource_id
   path_part   = each.value.path_part
 }
 
+resource "aws_api_gateway_resource" "level_2" {
+  for_each    = local.level_2
+  rest_api_id = var.rest_api_id
+  parent_id   = aws_api_gateway_resource.level_1[each.value.parent_key].id
+  path_part   = each.value.path_part
+}
 
-locals {
-  resource_ids_with_root = merge(
-    { "__root__" = var.root_resource_id },
-    aws_api_gateway_resource.this != null
-    ? { for k, r in aws_api_gateway_resource.this : k => r.id }
-    : {}
-  )
+resource "aws_api_gateway_resource" "level_3" {
+  for_each    = local.level_3
+  rest_api_id = var.rest_api_id
+  parent_id   = aws_api_gateway_resource.level_2[each.value.parent_key].id
+  path_part   = each.value.path_part
+}
+
+resource "aws_api_gateway_resource" "level_4" {
+  for_each    = local.level_4
+  rest_api_id = var.rest_api_id
+  parent_id   = aws_api_gateway_resource.level_3[each.value.parent_key].id
+  path_part   = each.value.path_part
+}
+
+resource "aws_api_gateway_resource" "level_5" {
+  for_each    = local.level_5
+  rest_api_id = var.rest_api_id
+  parent_id   = aws_api_gateway_resource.level_4[each.value.parent_key].id
+  path_part   = each.value.path_part
 }
--- aws_api_gateway_rest_api/submodules/resources/outputs.tf
+++ aws_api_gateway_rest_api/submodules/resources/outputs.tf
@@ -1,3 +1,9 @@
 output "resource_ids" {
-  value = { for k, v in aws_api_gateway_resource.this : k => v.id }
+  value = merge(
+    { for k, r in aws_api_gateway_resource.level_1 : k => r.id },
+    { for k, r in aws_api_gateway_resource.level_2 : k => r.id },
+    { for k, r in aws_api_gateway_resource.level_3 : k => r.id },
+    { for k, r in aws_api_gateway_resource.level_4 : k => r.id },
+    { for k, r in aws_api_gateway_resource.level_5 : k => r.id },
+  )
 }
